44888befd00fced8f24c4c7fd4d560c5ead5728a
hem6 <hem6@users.noreply.github.com>
2020-04-01 00:11:20 +0900

    Solve ABC159 A, B

---
+++ abc159/src/bin/b.rs
@@ -0,0 +1,74 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        s:chars,
+    }
+
+    let n = s.len();
+    let first_half = &s[0..(n - 1) / 2];
+    let second_half = &s[(n + 3) / 2 - 1..n];
+
+    if first_half != second_half {
+        println!("No");
+        return;
+    }
+
+    let mut reversed = vec!['a'; (n - 1) / 2];
+    reversed.copy_from_slice(first_half);
+    reversed.reverse();
+
+    if first_half != &reversed[..] {
+        println!("No");
+        return;
+    }
+
+    println!("Yes");
+}
