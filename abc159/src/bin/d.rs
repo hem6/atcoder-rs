b5e48e1258143fea60271e7bf40ab02ff29acd97
hem6 <hem6@users.noreply.github.com>
2020-04-02 21:04:29 +0900

    Solve ABC159 D

---
+++ abc159/src/bin/d.rs
@@ -0,0 +1,69 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::collections::HashMap;
+
+fn main() {
+    input! {
+        n: usize,
+        a: [u64; n]
+    }
+    let mut hm: HashMap<u64, u64> = HashMap::new();
+
+    for i in &a {
+        *hm.entry(*i).or_insert(0) += 1;
+    }
+
+    let all_combinations = hm.iter().fold(0, |acc, (_, v)| acc + v * (v - 1) / 2);
+
+    for i in 0..n {
+        let num = hm.get(&a[i]).unwrap();
+        println!("{}", all_combinations - (num - 1));
+    }
+}
