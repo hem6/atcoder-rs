1535e2b9f80008351407d9a1d91f85829b8bedbd
hem6 <hem6@users.noreply.github.com>
2020-04-13 21:56:05 +0900

    Solve ABC157 D with UnionFind

--- abc157/src/bin/d.rs
+++ abc157/src/bin/d.rs
@@ -10,12 +10,15 @@ fn main() {
         b: [[Usize1;2];k],
     }
 
-    let mut friend_graph: Vec<Vec<usize>> = vec![vec![]; n];
+    let mut uf = UnionFind::new(n);
+    let mut deg = vec![0; n];
 
     for v in f {
         let (from, to) = (v[0], v[1]);
-        friend_graph[from].push(to);
-        friend_graph[to].push(from);
+
+        deg[from] += 1;
+        deg[to] += 1;
+        uf.unite(from, to);
     }
 
     let mut block_graph: Vec<Vec<usize>> = vec![vec![]; n];
@@ -26,42 +29,56 @@ fn main() {
         block_graph[to].push(from);
     }
 
-    let mut cluster: Vec<i32> = vec![-1; n];
-    let mut cluster_no = 0;
-
     for i in 0..n {
-        if cluster[i] != -1 {
-            continue;
-        }
-
-        dfs(i, &friend_graph, &mut cluster, cluster_no);
-        cluster_no += 1;
-    }
-
-    let mut cluster_member_count = vec![0; cluster_no as usize];
-    for i in 0..n {
-        cluster_member_count[cluster[i] as usize] += 1;
-    }
-
-    for i in 0..n {
-        let total = cluster_member_count[cluster[i] as usize];
-        let direct_friend = friend_graph[i].len();
-        let blocked = block_graph[i]
-            .iter()
-            .filter(|&v| cluster[*v as usize] == cluster[i])
-            .count();
+        let root = uf.root(i);
+        let total = uf.size[root];
+        let direct_friend = deg[i];
+        let blocked = block_graph[i].iter().filter(|&b| uf.same(i, *b)).count();
         let ans = total - direct_friend - blocked - 1;
         println!("{}", ans);
     }
 }
 
-fn dfs(i: usize, g: &Vec<Vec<usize>>, cluster: &mut Vec<i32>, cluster_no: i32) {
-    cluster[i] = cluster_no;
-    // なんらかの処理(行きがけ順)
-    for &next in &g[i] {
-        if cluster[next] != -1 {
-            continue;
+struct UnionFind {
+    par: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(i: usize) -> Self {
+        UnionFind {
+            par: (0..i).collect(),
+            size: vec![1; i],
         }
-        dfs(next, g, cluster, cluster_no);
+    }
+
+    fn root(&mut self, i: usize) -> usize {
+        if self.par[i] == i {
+            return i;
+        } else {
+            self.par[i] = self.root(self.par[i]);
+            return self.par[i];
+        }
+    }
+
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        self.root(x) == self.root(y)
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.root(x);
+        let y = self.root(y);
+
+        if x == y {
+            return;
+        }
+
+        if self.size[x] < self.size[y] {
+            self.par[x] = y;
+            self.size[y] += self.size[x];
+        } else {
+            self.par[y] = x;
+            self.size[x] += self.size[y];
+        }
     }
 }
