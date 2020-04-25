8c43350a94c6de86f4fe395e5044944b8adc8e25
hem6 <hem6@users.noreply.github.com>
2020-04-25 16:22:50 +0900

    Solve ABC153 A

---
+++ abc153/src/bin/a.rs
@@ -0,0 +1,15 @@
+#![allow(unused_imports)]
+use itertools::Itertools;
+use proconio::{input, marker::*};
+use std::cmp::*;
+use std::collections::*;
+use superslice::*;
+
+fn main() {
+    input! {
+        h: f64,
+        a: f64,
+    }
+
+    println!("{}", (h / a).ceil());
+}
